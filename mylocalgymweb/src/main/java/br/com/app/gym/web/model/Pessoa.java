b227a5e4b8c99662538278e743571e64b5b508c5
Matheus <mathgunz@gmail.com>
2016-03-30 17:12:21 -0300

    init

---
+++ mylocalgymweb/src/main/java/br/com/app/gym/web/model/Pessoa.java
@@ -0,0 +1,21 @@
+package br.com.app.gym.web.model;
+
+public class Pessoa {
+	
+	private String nomePessoa;
+	private String sobrenomePessoa;
+	
+	public String getNomePessoa() {
+		return nomePessoa;
+	}
+	public void setNomePessoa(String nomePessoa) {
+		this.nomePessoa = nomePessoa;
+	}
+	public String getSobrenomePessoa() {
+		return sobrenomePessoa;
+	}
+	public void setSobrenomePessoa(String sobrenomePessoa) {
+		this.sobrenomePessoa = sobrenomePessoa;
+	}
+
+}
