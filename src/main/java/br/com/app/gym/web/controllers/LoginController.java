476dcc598ea6ffdf0fa095b7b83ed721b50197d6
matheus <mathgunz@gmail.com>
2016-08-05 22:45:44 -0300

    pacote de cadastro

---
+++ src/main/java/br/com/app/gym/web/controllers/LoginController.java
@@ -0,0 +1,11 @@
+package br.com.app.gym.web.controllers;
+
+import javax.enterprise.inject.Model;
+import javax.faces.bean.RequestScoped;
+
+
+@Model
+@RequestScoped
+public class LoginController {
+
+}
