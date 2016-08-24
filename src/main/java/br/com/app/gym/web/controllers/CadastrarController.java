5d31ed2969b83df5dab5c70a79b7dd46eb751386
RGC <RGC@DESKTOP-J56QUFN>
2016-08-23 21:58:43 -0300

    Inserido botão salvar no service

--- src/main/java/br/com/app/gym/web/controllers/CadastrarController.java
+++ src/main/java/br/com/app/gym/web/controllers/CadastrarController.java
@@ -3,18 +3,22 @@ package br.com.app.gym.web.controllers;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
 
 import br.com.app.gym.web.model.PessoaJuridica;
+import br.com.app.gym.web.service.ClienteService;
 
 @Model
 @RequestScoped
 public class CadastrarController {
 	
+	@Inject
+	private ClienteService clienteService;
+	
 	private PessoaJuridica pessoa;
 
 	@PostConstruct
 	public void init() {
-		
 		this.pessoa = new PessoaJuridica();
 
 	}
@@ -25,6 +29,9 @@ public class CadastrarController {
 
 	public void setPessoa(PessoaJuridica pessoa) {
 		this.pessoa = pessoa;
+	}
+	public void cadastrarPessoaJuridica(){
+		this.clienteService.cadastrarCliente(pessoa);
 	}
 
 }
---
+++ src/main/java/br/com/app/gym/web/service/ClienteService.java
@@ -0,0 +1,9 @@
+package br.com.app.gym.web.service;
+
+import br.com.app.gym.web.model.PessoaJuridica;
+
+public interface ClienteService {
+	
+	boolean cadastrarCliente(PessoaJuridica pessoaJuridica);
+
+}
