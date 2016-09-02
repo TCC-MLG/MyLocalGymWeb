6be74c13e49aa636bd0e4bf1999aae3c0e651535
RGC <RGC@DESKTOP-J56QUFN>
2016-09-01 22:40:16 -0300

    asd

--- src/main/java/br/com/app/gym/web/service/impl/LoginServiceImpl.java
+++ src/main/java/br/com/app/gym/web/service/impl/LoginServiceImpl.java
@@ -1,8 +1,8 @@
 package br.com.app.gym.web.service.impl;
 
+import br.com.app.gym.web.model.Academia;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
 
 import br.com.app.gym.web.model.Login;
 import br.com.app.gym.web.service.LoginService;
@@ -10,12 +10,13 @@ import java.text.MessageFormat;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class LoginServiceImpl implements LoginService {
 
     private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://localhost:8080/mylocalgym//resources";
+    private static final String BASE_URI = "http://localhost:8080/mylocalgym/resources";
 
     public LoginServiceImpl() {
         client = ClientBuilder.newClient();
@@ -25,11 +26,11 @@ public class LoginServiceImpl implements LoginService {
     @Override
     public boolean efetuarLogin(Login login) throws ClientErrorException {
         WebTarget resource = webTarget;
-        
-        resource = resource.path(MessageFormat.format("autenticar/{0}/{1}", new Object[]{login.getUsuario(), login.getSenha()}));
-
+        resource = resource.path(MessageFormat.format("academia/{0}/{1}", new Object[]{login.getUsuario(), login.getSenha()}));
         Response response = resource.request(MediaType.APPLICATION_JSON).get();
-
+        
+        Academia academia = response.readEntity(Academia.class);
+        
         return false;
     }
 
